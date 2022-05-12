bb036ef4ee02ef866f4ff5dba2cebf2617657e10
nobumitsu_masugata <ys.training220208@gmail.com>
2022-05-13 00:21:28 +0900

    create templates/SideBar

--- lib/type.ts
+++ lib/type.ts
@@ -1,3 +1,16 @@
-export type Article = {
-  
+export type UserData = {
+  img: string;
+  name: string;
+  text: string;
+};
+
+export type RecommendArticle = {
+  id: number;
+  img: string;
+  title: string;
+};
+
+export type RecommendArticleCategory = {
+  id: number;
+  title: string;
 };
