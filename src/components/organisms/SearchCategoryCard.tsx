bf6e0a2b5d6db26b2d5bd2b4f5901e8a2a9a0158
nobumitsu_masugata <ys.training220208@gmail.com>
2022-05-15 21:19:36 +0900

    add organism component for search

---
+++ src/components/organisms/SearchCategoryCard.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { headerDatas } from '../../../lib/datas'
+import { FlexCenter } from '../../styles/styled-components'
+import { Paper } from '../atoms'
+import { LinkList, TitleInPaper } from '../molecules'
+
+const SearchCategoryCard = () => {
+  return (
+    <Paper padding='40px 50px'>
+      <TitleInPaper text="カテゴリから検索"/>
+      <LinkList links={headerDatas}/>      
+    </Paper>
+  )
+}
+
+export default SearchCategoryCard
---
+++ src/stories/organisms/SearchCategoryCard.stories.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import "../../styles/globals.css";
+import styled from "styled-components";
+import { SearchCategoryCard } from "../../components/organisms";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+	title: "Organisms/Search",
+	component: SearchCategoryCard,
+	// More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+	argTypes: {
+		backgroundColor: { control: "color" },
+	},
+} as ComponentMeta<typeof SearchCategoryCard>;
+
+export const StyledSearchCategoryCard: ComponentStory<typeof SearchCategoryCard> = args => (
+	<SearchCategoryCard />
+);
