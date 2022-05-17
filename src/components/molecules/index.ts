72e1395c8bd4861b1539dc09e48e9ac39224b334
nobumitsu_masugata <ys.training220208@gmail.com>
2022-05-17 22:48:19 +0900

    add InputArea component

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -7,6 +7,7 @@ export { default as FooterList1 } from "./FooterList1";
 export { default as FooterList2 } from "./FooterList2";
 export { default as HeaderLink } from "./HeaderLink";
 export { default as IconInput } from "./IconInput";
+export { default as InputArea } from "./InputArea";
 export { default as LinkList } from "./LinkList";
 export { default as Title } from "./Title";
 export { default as TitleInPaper } from "./TitleInPaper";
