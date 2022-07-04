14f0f36805dddb2b2379f35f673cab4dd14c073b
nobumitsu_masugata <ys.training220208@gmail.com>
2022-07-04 22:34:59 +0900

    add: google analyticsと連結

---
+++ src/components/atoms/GoogleAnalytics.tsx
@@ -0,0 +1,17 @@
+import Script from 'next/script'
+
+const GoogleAnalytics = () => (
+  <>
+    <Script defer src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_KEY}`} strategy="afterInteractive" />
+    <Script id="ga" defer strategy="afterInteractive">
+      {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());    
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_KEY}');
+      `}
+    </Script>
+  </>
+)
+
+export default GoogleAnalytics
--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -2,6 +2,7 @@ export { default as ArrowIcon } from "./ArrowIcon";
 export { default as Border } from "./Border";
 export { default as Button } from "./Button";
 export { default as Date } from "./Date";
+export { default as GoogleAnalytics } from "./GoogleAnalytics";
 export { default as Input } from "./Input";
 export { default as StyledLink } from "./Link";
 export { default as Paper } from "./Paper";
